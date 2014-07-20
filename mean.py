04a7e5453c54af0993f52aef71cfc7a37b0bb95f
Young Lee <leeeun@umich.edu>
2014-07-20 10:26:21 -0400

    changed num to number

--- mean.py
+++ mean.py
@@ -7,8 +7,8 @@ import sys
 sum = 0.0
 n = 0.0 #initialize value
 
-for num in sys.stdin:  #take information from the standard input line 
- 	sum += float(num)  
+for number in sys.stdin:  #take information from the standard input line 
+ 	sum += float(number)  
 	n += 1.0
 
 print sum/n 
