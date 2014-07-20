212b70e266c67219477b468dfefc8093c421a92c
Young Lee <leeeun@umich.edu>
2014-07-20 12:18:17 -0400

    change

--- mean.py
+++ mean.py
@@ -8,9 +8,9 @@ sum = 0.0
 n = 0.0 #initialize value
 
 
-for number1 in sys.stdin:  #take information from the standard input 
-line 
- 	sum += float(number1)  
+for number_1 in sys.stdin:  #take information from the standard input 
+
+ 	sum += float(number_1)  
 	n += 1.0
 
 print sum/n 
